eefe231217d2faa87cbf4cc78425aaa326776d36
Jeffreykakk <1920466903@qq.com>
2023-08-22 21:08:28 +0800

    8-22

---
+++ LeetCode-isHappy1.0/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <windows.h>
+#include <set>
+
+using namespace std;
+
+// 编写一个算法来判断一个数 n 是不是快乐数
+
+// 「快乐数」 定义为：
+
+// 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+// 然后重复这个过程直到这个数变为 1，也可能是 无限循环但始终变不到 1
+// 如果这个过程 结果为 1，那么这个数就是快乐数。
+// 如果 n 是 快乐数 就返回 true ；不是，则返回 false
+
+class Solution
+{
+    set<int> s;
+
+public:
+    bool isHappy(int n)
+    {
+        int sum = 0;
+        while (n)
+        {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        if (sum == 1)
+            return true;
+        if (s.insert(sum).second == false)
+            return false;
+        else
+            return isHappy(sum);
+    }
+};
+
+int main()
+{
+    cout << Solution().isHappy(2) << endl;
+    system("pause");
+    return 0;
+}
