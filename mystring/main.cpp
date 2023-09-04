aec8172cd73294e077bd43795ac1b2d653d45138
Jeffreykakk <1920466903@qq.com>
2023-09-04 17:01:11 +0800

    9-4

--- mystring/main.cpp
+++ mystring/main.cpp
@@ -2,21 +2,55 @@
 #include <windows.h>
 #include "mystring.h"
 
-void test_string()
+void test1()
+{
+    string s1 = "abcd";
+    s1.append("efghijk");
+    const string s2 = "cccccc";
+    s1.append(s2);
+    s1.push_back('a');
+    cout << s1.append(5, '6').c_str() << endl;
+    cout << s1.insert(2, "8888").c_str() << endl;
+    cout << s1.insert(2, s2).c_str() << endl;
+    cout << s1.insert(2, 3, '3').c_str() << endl;
+    cout << s1.erase(3, 3).c_str() << endl;
+    s1.clear();
+    s1 += "abcd";
+    s1 += s2;
+    s1 += 'h';
+    cout << s1.c_str() << endl;
+    cout << s1.find(s2) << endl;
+    string s3 = s1.substr(3, 2);
+    cout << s3.c_str() << endl;
+}
+void test2()
+{
+    string s = "abcdef";
+    cout << s.substr(2, 2).c_str() << endl;
+    s.resize(10, 'c');
+    cout << s.c_str() << endl;
+}
+void test3()
+{
+    string s1 = "abcd";
+    string s2 = "1234";
+    cout << s1 << s2 << endl;
+}
+
+void test4()
 {
     string s1;
     string s2;
-    cin >> s1;
-    cin >> s2;
+    cin >> s1 >> s2;
     cout << s1 << endl;
     cout << s2 << endl;
 }
 
 int main()
 {
-    Jeffrey::test_string();
+    Jeffrey::test4();
     cout << "------------" << endl;
-    ::test_string();
+    ::test4();
 
     system("pause");
     return 0;
--- mystring/mystring.h
+++ mystring/mystring.h
@@ -9,11 +9,51 @@ using namespace std;
 
 namespace Jeffrey
 {
+    template <class iterator, class Ref>
+    class __reverse_iterator
+    {
+    public:
+        typedef __reverse_iterator<iterator, Ref> Self;
+        __reverse_iterator(const iterator &it)
+            : _it(it)
+        {
+        }
+        Self &operator++()
+        {
+            --_it;
+            return *this;
+        }
+        Self &operator--()
+        {
+            ++_it;
+            return *this;
+        }
+        Ref operator*()
+        {
+            iterator tmp = _it;
+            --tmp;
+            return *tmp;
+        }
+        bool operator==(const Self &rit)
+        {
+            return _it == rit._it;
+        }
+        bool operator!=(const Self &rit)
+        {
+            return _it != rit._it;
+        }
+
+    private:
+        iterator _it;
+    };
+
     class string
     {
     public:
         typedef char *iterator;
         typedef const char *const_iterator;
+        typedef __reverse_iterator<iterator, char &> reverse_iterator;
+        typedef __reverse_iterator<const_iterator, const char &> const_reverse_iterator;
 
         // 迭代器
         iterator begin()
@@ -35,7 +75,22 @@ namespace Jeffrey
         {
             return _str + _size;
         }
-
+        reverse_iterator rbegin()
+        {
+            return end();
+        }
+        const_reverse_iterator rbegin() const
+        {
+            return end();
+        }
+        reverse_iterator rend()
+        {
+            return begin();
+        }
+        const_reverse_iterator rend() const
+        {
+            return begin();
+        }
         size_t size() const
         {
             return _size;
@@ -72,31 +127,39 @@ namespace Jeffrey
         }
 
         // 默认成员函数
-        string(const char *s = "")
+        string(const char *str = "")
         {
-            assert(s);
-            _size = strlen(s);
+            assert(str);
+            _size = strlen(str);
             _capacity = _size;
             _str = new char[_size + 1];
-            strcpy(_str, s);
+            strcpy(_str, str);
         }
-
-        string(const string &s)
+        string(size_t n, const char &ch)
+        {
+            _str = new char[n + 1];
+            for (size_t i = 0; i < n; i++)
+                _str[i] = ch;
+            _size = _capacity = n;
+            _str[_size] = '\0';
+        }
+        string(const string &str)
             : _str(nullptr), _size(0), _capacity(0)
         {
-            string tmp(s._str);
+            string tmp(str._str);
             swap(tmp);
         }
 
         ~string()
         {
             delete[] _str;
+            _str = nullptr;
             _size = _capacity = 0;
         }
 
-        string &operator=(string s)
+        string &operator=(string str)
         {
-            swap(s);
+            swap(str);
             return *this;
         }
 
@@ -121,7 +184,6 @@ namespace Jeffrey
             {
                 for (size_t i = _size; i < n; i++)
                     _str[i] = ch;
-                _str[n] = '\0';
                 _size = n;
             }
             else
@@ -143,63 +205,69 @@ namespace Jeffrey
         {
             return _size == 0;
         }
-        void append(const char *s)
+        string &append(const char *str)
         {
-            assert(s);
-            int len = strlen(s);
+            assert(str);
+            size_t len = strlen(str);
             if (_size + len > _capacity)
                 reserve(_size + len);
-            strncpy(_str + _size, s, len);
+            strcpy(_str + _size, str);
             _size += len;
-            _str[_size] = '\0';
+            return *this;
         }
-        string &operator+=(char ch)
+        string &append(const string &str)
+        {
+            return append(str._str);
+        }
+        string &append(size_t n, const char &ch)
+        {
+            for (size_t i = 0; i < n; i++)
+                push_back(ch);
+            return *this;
+        }
+        string &operator+=(const char *str)
+        {
+            return append(str);
+        }
+        string &operator+=(const char &ch)
         {
             push_back(ch);
             return *this;
         }
-        string &operator+=(const char *s)
+        string &operator+=(const string &str)
         {
-            append(s);
-            return *this;
+            return append(str);
         }
-        string &insert(size_t pos, char ch)
+        string &insert(size_t pos, const char *str)
         {
             assert(pos <= _size);
-            if (_size == _capacity)
-                reserve(_capacity == 0 ? 4 : _capacity * 2);
-            int end = size() + 1;
-            while (pos < end)
-            {
-                _str[end] = _str[end - 1];
-                end--;
-            }
-            _str[pos] = ch;
-            _size++;
-            return *this;
-        }
-        string &insert(size_t pos, const char *s)
-        {
-            assert(s);
-            assert(pos <= _size);
-            int len = strlen(s);
+            size_t len = strlen(str);
             if (_size + len > _capacity)
                 reserve(_size + len);
-            memmove(_str + pos + len, _str + pos, sizeof(char) * (_size - pos + 1));
-            strncpy(_str + pos, s, len);
+            memmove(_str + pos + len, _str + pos, _size - pos + 1);
+            strncpy(_str + pos, str, len);
             _size += len;
             return *this;
         }
+        string &insert(size_t pos, const string &str)
+        {
+            return insert(pos, str._str);
+        }
+        string &insert(size_t pos, size_t n, const char &ch)
+        {
+            string str(n, ch);
+            return insert(pos, str);
+        }
         string &erase(size_t pos, size_t len = npos)
         {
-            if (pos + len >= _size || len == npos)
+            if (len == npos || pos + len >= _size)
             {
                 _str[pos] = '\0';
-                _size -= len;
+                _size = pos;
             }
             else
             {
-                memmove(_str + pos, _str + pos + len, sizeof(char) * (_size - pos - len + 1));
+                memmove(_str + pos, _str + pos + len, _size - pos - len + 1);
                 _size -= len;
             }
             return *this;
@@ -211,7 +279,21 @@ namespace Jeffrey
             _str[_size] = '\0';
         }
 
-        size_t find(char ch, size_t pos = 0) const
+        size_t find(const char *str, size_t pos = 0) const
+        {
+            assert(str);
+            assert(pos < _size);
+            char *ret = strstr(_str + pos, str);
+            if (ret)
+                return ret - _str;
+            else
+                return npos;
+        }
+        size_t find(const string &str, size_t pos = 0) const
+        {
+            return find(str._str);
+        }
+        size_t find(const char &ch, size_t pos = 0) const
         {
             assert(pos < _size);
             for (size_t i = pos; i < _size; i++)
@@ -221,73 +303,57 @@ namespace Jeffrey
             }
             return npos;
         }
-        size_t find(const char *s, size_t pos = 0) const
-        {
-            assert(pos < _size);
-            assert(s);
-            char *ret = strstr(_str + pos, s);
-            if (ret == nullptr)
-                return npos;
-            else
-                return ret - _str;
-            return npos;
-        }
         string substr(size_t pos = 0, size_t len = npos) const
         {
-            string sub;
             if (len == npos || pos + len >= _size)
-            {
-                sub.reserve(_size - pos);
-                strcpy(sub._str, _str + pos);
-                sub._size = _size - pos;
-            }
+                return string(_str + pos);
             else
             {
-                sub.reserve(len);
-                strncpy(sub._str, _str + pos, len);
+                string str;
+                str.reserve(len);
+                strncpy(str._str, _str + pos, len);
+                str._size += len;
+                return str;
             }
-            return sub;
         }
-        bool operator==(const string &s) const
+        bool operator==(const string &str) const
         {
-            return strcmp(_str, s._str) == 0;
+            return strcmp(_str, str._str) == 0;
         }
-        bool operator>=(const string &s) const
+        bool operator>=(const string &str) const
         {
-            return *this > s || *this == s;
+            return *this > str || *this == str;
         }
-        bool operator<=(const string &s) const
+        bool operator<=(const string &str) const
         {
-            return !(*this > s);
+            return !(*this > str);
         }
-        bool operator>(const string &s) const
+        bool operator>(const string &str) const
         {
-            return strcmp(_str, s._str) > 0;
+            return strcmp(_str, str._str) > 0;
         }
-        bool operator<(const string &s) const
+        bool operator<(const string &str) const
         {
-            return !(*this >= s);
+            return !(*this >= str);
         }
-        bool operator!=(const string &s) const
+        bool operator!=(const string &str) const
         {
-            return !(*this == s);
+            return !(*this == str);
         }
 
     private:
         char *_str;
         size_t _size;
         size_t _capacity;
-        const static size_t npos;
+        const static size_t npos = -1;
     };
-    const size_t string::npos = -1;
 
-    ostream &operator<<(ostream &out, const string &s)
+    ostream &operator<<(ostream &out, const string &str)
     {
-        for (auto &e : s)
-            cout << e;
+        out << str.c_str();
         return out;
     }
-    istream &operator>>(istream &in, string &s)
+    istream &operator>>(istream &in, string &str)
     {
         char ch;
         while (1)
@@ -296,31 +362,63 @@ namespace Jeffrey
             if (ch != ' ' && ch != '\n')
                 break;
         }
-        char buf[16];
+        char buf[33];
         size_t i = 0;
         while (ch != '\n')
         {
             buf[i++] = ch;
-            if (i == 15)
+            ch = in.get();
+            if (i == 32)
             {
                 buf[i] = '\0';
                 i = 0;
-                s += buf;
+                str += buf;
             }
-            ch = in.get();
         }
         buf[i] = '\0';
-        s += buf;
-
+        str += buf;
         return in;
     }
 
-    void test_string()
+    void test1()
+    {
+        string s1 = "abcd";
+        s1.append("efghijk");
+        const string s2 = "cccccc";
+        s1.append(s2);
+        s1.push_back('a');
+        cout << s1.append(5, '6').c_str() << endl;
+        cout << s1.insert(2, "8888").c_str() << endl;
+        cout << s1.insert(2, s2).c_str() << endl;
+        cout << s1.insert(2, 3, '3').c_str() << endl;
+        cout << s1.erase(3, 3).c_str() << endl;
+        s1.clear();
+        s1 += "abcd";
+        s1 += s2;
+        s1 += 'h';
+        cout << s1.c_str() << endl;
+        cout << s1.find(s2) << endl;
+        string s3 = s1.substr(3, 2);
+        cout << s3.c_str() << endl;
+    }
+    void test2()
+    {
+        string s = "abcdef";
+        cout << s.substr(2, 2).c_str() << endl;
+        s.resize(10, 'c');
+        cout << s.c_str() << endl;
+    }
+    void test3()
+    {
+        string s1 = "abcd";
+        string s2 = "1234";
+        cout << s1 << s2 << endl;
+    }
+    void test4()
     {
         string s1;
         string s2;
-        cin >> s1;
-        cin >> s2;
+        cin >> s1 >> s2;
         cout << s1 << endl;
         cout << s2 << endl;
     }
